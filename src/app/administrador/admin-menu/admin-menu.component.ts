ad28cefa8afa3a982a0cfbf140d1d47f81894713
Skar13F <of5162@gmail.com>
2023-12-27 17:35:50 -0600

    se agregó el commonModule, esto para poder usarlo en otros componentes

--- src/app/administrador/admin-menu/admin-menu.component.ts
+++ src/app/administrador/admin-menu/admin-menu.component.ts
@@ -3,11 +3,12 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-menu',
   standalone: true,
-  imports: [MatListModule,MatIconModule,NgbCollapseModule],
+  imports: [MatListModule,MatIconModule,NgbCollapseModule,CommonModule],
   templateUrl: './admin-menu.component.html',
   styleUrl: './admin-menu.component.css'
 })
