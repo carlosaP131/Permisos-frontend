c8490dae5d5fd232231e13087908c0d607a19779
NancyObed <nancyobed11@gmail.com>
2024-01-07 22:54:25 -0600

    On branch Login Changes to be committed: 	modified:   src/app/app.routes.ts 	modified:   src/app/login/login.component.css      modified:   src/app/login/login.component.html 	modified:   src/app/login/login.component.ts 	new file:   src/assets/img/fondo.jpeg 	new file:   src/assets/img/logo.png

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { AdministradorComponent } from './administrador/administrador.component';
+import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
-    {path:"",component:AdministradorComponent}
+    {path:"",component:AdministradorComponent},
+    {path:"login",component:LoginComponent}
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule,MatInputModule,MatButtonModule,MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
